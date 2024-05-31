2b03e566769c747e75d1162aceeb8c36a45f615d
Pooranjoy <pooranjoyb2016@gmail.com>
2024-05-31 14:51:08 +0530

    Applied migrations to postgres-docker and completed Signup

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,14 +1,29 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthDto } from './dto';
+import * as argon from 'argon2';
 
 @Injectable({})
 export class AuthService {
   constructor(private prisma: PrismaService) {}
-  signup() {
-    return { msg: 'Signin' };
+  async signup(dto: AuthDto) {
+    const hash = await argon.hash(dto.password);
+
+    const user = await this.prisma.user.create({
+      data: {
+        email: dto.email,
+        firstName: dto.firstName,
+        lastName: dto.lastName,
+        hash,
+      },
+    });
+
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const { hash: _, ...userWithoutHash } = user;
+    return userWithoutHash;
   }
 
   signin() {
-    return { msg: 'Signup' };
+    return { msg: 'Signin' };
   }
 }
--- src/auth/dto/auth.dto.ts
+++ src/auth/dto/auth.dto.ts
@@ -9,4 +9,10 @@ export class AuthDto {
   @IsString()
   @IsNotEmpty()
   password: string;
+
+  @IsString()
+  firstName: string;
+
+  @IsString()
+  lastName: string;
 }
